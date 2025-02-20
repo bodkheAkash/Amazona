df1e730f4f6d17daa92fafe168370aab2380a87b
akashbodakhe <akashbodakhe99@gmail.com>
2025-02-20 23:51:13 +0530

    change type

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,14 +5,14 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const formatNumberWithDecimal = (num: Number): string => {
+export const formatNumberWithDecimal = (num: number): string => {
   const [int, decimal] = num.toString().split(".")
   return decimal ? `${int}.${decimal.padEnd(2, "0")}` : int
 }
 
 export const toSlug = (text: string): string =>
-text
-  .toLowerCase() // Convert to lowercase
-  .replace(/[^\w\s-]/g, '') // Remove non-word, non-whitespace, non-hyphen characters
-  .replace(/\s+/g, '-') // Replace whitespace with hyphens
-  .replace(/^-+|-+$/g, ''); // Trim leading and trailing hyphe
+  text
+    .toLowerCase() // Convert to lowercase
+    .replace(/[^\w\s-]/g, "") // Remove non-word, non-whitespace, non-hyphen characters
+    .replace(/\s+/g, "-") // Replace whitespace with hyphens
+    .replace(/^-+|-+$/g, "") // Trim leading and trailing hyphe
